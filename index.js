a1694cb60db4efa960241e0840bb0f10de4377d7
Adriano lamas <89049143+Allrodk@users.noreply.github.com>
2021-08-21 04:57:18 +0000

    altered

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ O programa deve no final emitir uma classificação sobre a participação da pe
 Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
 */
 let resposta, qtdeSim = 0;
-const perguntas = ['1 - Telefonou para a vítima? Sim(1) ou Não(0)?',
-                   '2 - Esteve no local do crime? Sim(1) ou Não(0)?',
-                   '3 - Mora perto da vítima? Sim(1) ou Não(0)?',
-                   '4 - Devia para a vítima? Sim(1) ou Não(0)?',
-                   '5 - Já trabalhou com a vítima? Sim(1) ou Não(0)?'
+const perguntas = ['1 - Telefonou para a vítima [S/N]? ',
+                   '2 - Esteve no local do crime [S/N])?',
+                   '3 - Mora perto da vítima [S/N]?',
+                   '4 - Devia para a vítima [S/N]',
+                   '5 - Já trabalhou com a vítima [S/N]?'
                   ];
 
-console.log('Atenção, responda 1 para SIM e 0 para NÃO.\n');                 
+console.log('Interrogatório, responda apenas [S/N].\n');                 
 for (let i = 0; i <= perguntas.length-1; i++){  
   resposta = prompt(perguntas[i]);
-  if (resposta != 1 && resposta != 0 || resposta == ''){
-    console.log('\nResponda apenas 1 ou 0.');
+  if (resposta.toUpperCase() != 'S' && resposta.toUpperCase() != 'N'){
+    console.log('\nResponda apenas [S/N].');
     i--
   }else{
-    if (resposta == 1){
+    if (resposta.toUpperCase() == 'S'){
       qtdeSim++;
     }
   }
