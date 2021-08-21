14f2fdecbcfa20794cb9c6dff1affc880906d896
Adriano lamas <89049143+Allrodk@users.noreply.github.com>
2021-08-21 04:16:07 +0000

    altered

--- Projeto1.js
+++ Projeto1.js
@@ -11,8 +11,7 @@ Faça um programa que faça 5 perguntas para uma pessoa sobre um crime. As pergu
 O programa deve no final emitir uma classificação sobre a participação da pessoa no crime. 
 Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita", entre 3 e 4 como "Cúmplice" e 5 como "Assassino". Caso contrário, ele será classificado como "Inocente".
 */
-let qtdeSim = 0;
-let respostas = [];
+let resposta, qtdeSim = 0;
 const perguntas = ['1 - Telefonou para a vítima? Sim(1) ou Não(0)?',
                    '2 - Esteve no local do crime? Sim(1) ou Não(0)?',
                    '3 - Mora perto da vítima? Sim(1) ou Não(0)?',
@@ -22,12 +21,12 @@ const perguntas = ['1 - Telefonou para a vítima? Sim(1) ou Não(0)?',
 
 console.log('Atenção, responda 1 para SIM e 0 para NÃO.\n');                 
 for (let i = 0; i <= perguntas.length-1; i++){  
-  respostas[i] = prompt(perguntas[i]);
-  if (respostas[i] != 1 && respostas[i] != 0){
+  resposta = prompt(perguntas[i]);
+  if (resposta != 1 && resposta != 0 || resposta == ''){
     console.log('\nResponda apenas 1 ou 0.');
     i--
   }else{
-    if (respostas[i] == 1){
+    if (resposta == 1){
       qtdeSim++;
     }
   }
